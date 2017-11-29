b9d18c4496b313d7e63bc838a8df52504ad64f36
WY <weiyang_30@live.com>
2017-11-30 01:50:14 +0800

    Add Map

--- app/src/main/java/com/example/android/darktourism/Attraction.java
+++ app/src/main/java/com/example/android/darktourism/Attraction.java
@@ -1,6 +1,8 @@
 package com.example.android.darktourism;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,6 +12,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +26,7 @@ public class Attraction extends Fragment {
 
     DbHelper dbHelper;
     Button btnGetData;
+    Button btnGetMap;
     List<Places> listPlace = new ArrayList<Places>();
     List<Travel> listTravel = new ArrayList<Travel>();
     Spinner spinnerChoiceInfo;
@@ -37,6 +41,7 @@ public class Attraction extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_attraction, container, false);
         btnGetData = (Button) view.findViewById(R.id.btnGetData);
+        btnGetMap = (Button) view.findViewById(R.id.btnGetMap);
         //container = (LinearLayout)findViewById(R.id.container);
 
         dbHelper = new DbHelper(getActivity().getApplicationContext());
@@ -55,6 +60,29 @@ public class Attraction extends Fragment {
             @Override
             public void onClick(View view) {
                 onClickFindInfo(view);
+            }
+        });
+
+
+        view.findViewById(R.id.btnGetMap).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                spinnerChoiceInfo = (Spinner)mView.findViewById(R.id.spinnerAttractionChoiceInfo);
+                String myLocation = String.valueOf(spinnerChoiceInfo.getSelectedItem());
+                Uri.Builder builder = new Uri.Builder(); // Collect information and pass it to the method
+                builder.scheme("geo").opaquePart("0,0").appendQueryParameter("q",myLocation);
+
+                Uri geoLocation = builder.build();
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, geoLocation);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+
+
+
+/*
+                Toast.makeText(getActivity(), "This is my Toast message!",
+                        Toast.LENGTH_LONG).show();*/
+
             }
         });
 
